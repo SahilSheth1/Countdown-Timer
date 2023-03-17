dc6e42c1a51622642aa25667cd952ff547f732f0
LastBrainCells <sahilshethdevelopment@gmail.com>
2023-03-17 12:05:49 -0500

    Added Comments

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
+# Importing libraries
+
 import time
+
+# Prompt for user to enter time
 
 myTime = int(input("Enter the time in seconds: "))
 
+# For loop for countdown timer process
+
 for x in range(myTime, 0, -1):
-    seconds = x % 60
-    minutes = int(x / 60) % 60
-    hours = int(x / 3600) 
-    print(f"{hours:02}:{minutes:02}:{seconds:02}")
-    time.sleep(1)
+    seconds = x % 60 # Convert time to seconds
+    minutes = int(x / 60) % 60 # Convert time to minutes
+    hours = int(x / 3600)  # Convert time to hours
+    print(f"{hours:02}:{minutes:02}:{seconds:02}") # Print in time format
+    time.sleep(1) # Time sleep function
+
+# Prints when time is done
 
 print("TIME IS UP")
