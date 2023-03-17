3fe4e9bb292af91989263765a12002e9f1ccf34f
LastBrainCells <sahilshethdevelopment@gmail.com>
2023-03-16 19:47:33 -0500

    Added time format

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import time
 myTime = int(input("Enter the time in seconds: "))
 
 for x in range(myTime, 0, -1):
-    print(x)
+    seconds = x % 60
+    print(f"00:00:{seconds}")
     time.sleep(1)
 
 print("TIME IS UP")
